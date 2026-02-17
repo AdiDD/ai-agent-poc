828d11e011e4f98bb0add55328a68a3d94aab124
Adi Deaconu <adi.deaconu21@gmail.com>
2026-02-18 00:09:04 +0200

    feat: implement feedback loop

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = None
+    for _ in range(20):
+        response, messages = generate_content(client, messages, args)
+        if response:
+            break
+    
+    if response:
+        print('Final response:')
+        print(response.text)
+    else:
+        print('Could not get a response')
+
+def generate_content(client, messages, args):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
@@ -33,14 +46,12 @@ def main():
     )
 
     if args.verbose:
-        usage_metadata = response.usage_metadata
-        if usage_metadata is None:
-            raise RuntimeError(
-                "could not read api client response, something went wrong"
-            )
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {usage_metadata.candidates_token_count}")
+        print_response_metadata(response, args.user_prompt)
+
+    if response.candidates:
+        for c in response.candidates:
+            if c.content:
+                messages.append(c.content)
 
     # Print client response to console
     if response.function_calls:
@@ -57,11 +68,23 @@ def main():
                     f"Something went wrong during the {function_call.name} function call, with args {function_call.args}"
                 )
             function_results = [function_call_result.parts[0]]
+            messages.append(types.Content(role="user", parts=function_results))
             if args.verbose:
                 print(f"-> {function_call_result.parts[0].function_response.response}")
+        return None, messages
     else:
-        print("Response:")
-        print(response.text)
+        messages.append([types.Content(role="model", parts=[types.Part(text=response.text)])])
+        return response, messages
+
+
+
+def print_response_metadata(response, user_prompt):
+    usage_metadata = response.usage_metadata
+    if usage_metadata is None:
+        raise RuntimeError("could not read api client response, something went wrong")
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
