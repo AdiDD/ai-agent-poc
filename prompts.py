e3317cd4422798d3b0aaa15f1bd7d4a52d28abcc
Adi Deaconu <adi.deaconu21@gmail.com>
2026-03-01 13:35:46 +0200

    chore: enforce new line at the end of a file

--- prompts.py
+++ prompts.py
@@ -9,5 +9,4 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-When writing files, the file content should end with a new line character.
 """
